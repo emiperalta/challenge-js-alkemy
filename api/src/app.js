b849a860ed3753649fba4bc764d47693e3a27866
EmiPeralta <emilianop96@hotmail.com>
2021-05-13 17:13:08 -0300

    cors added

--- api/src/app.js
+++ api/src/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
 
 const operationRoutes = require('./routes/operation.routes');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(morgan('dev'));
